7917b3326741f79315ba453d3ec08c074ea078d9
Michel Figueira <michelfigueiratemp@gmail.com>
2021-08-12 15:12:14 -0300

    Finish Register User

--- src/app/home/register/register.component.ts
+++ src/app/home/register/register.component.ts
@@ -1,7 +1,10 @@
 import { User } from './../../models/user';
 import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { lowercaseValidator } from 'src/app/shared/lowercase/lowercase.validator';
+import { matchFieldsValidators } from 'src/app/shared/match/match.validators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,20 +14,32 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   newUserForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, userService: UserService) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
     this.newUserForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
-      userName: [''],
-      password: ['']
-    });
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(3)]],
+      userName: ['', [Validators.required, Validators.minLength(3), lowercaseValidator], [this.userService.userValid()]],
+      password: ['', [Validators.required, Validators.minLength(3)]]
+      },
+      { validators: [matchFieldsValidators] }
+    );
   }
 
   register() {
-    const newUser = this.newUserForm.getRawValue() as User;
-    console.log(newUser);
+    if (this.newUserForm.valid) {
+      const newUser = this.newUserForm.getRawValue() as User;
+      this.userService.register(newUser).subscribe(
+        () => {
+          this.router.navigate(['']);
+        },
+
+        (error) => {
+          console.log(error);
+        }
+      )
+    }
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,8 @@
 import { User } from './../models/user';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { first, map, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,23 @@ export class UserService {
 
   register(newUser: User) {
     return this.httpClient.post(`${this.baseURL}signup`, newUser);
+  }
+
+  checkUser(user: string) {
+    return this.httpClient.get(`${this.baseURL}exists/${user}`);
+  }
+
+  userValid(){
+    return (control: AbstractControl) => {
+      return control.valueChanges.pipe(
+        switchMap((username) =>
+          this.checkUser(username)
+        ),
+        map((userExist) => (userExist ? {userOk: true} : null)
+        ),
+        first()
+      );
+    }
   }
 
 }
